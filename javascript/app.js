ea0c7e58d9e2b3b0d8a8bc0dab80c1b97a2cf9fb
Amjad Ullah <amjadfast87@gmail.com>
2026-06-02 18:44:33 +0500

    Add initial project files including .gitignore, package.json, package-lock.json, and example scripts in JavaScript and Python

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+
+const name = "Amjad ullah";
+const age = 20;
+const foveriteLenguage = ["Javascript","Pyhton"];
+function MyInfo(){
+    console.log(`My name is ${name} Age : ${age} favorite programming language is : ${foveriteLenguage[0]} , ${foveriteLenguage[1]}`);
+    
+}
+
+
+MyInfo();
---
+++ python/main.py
@@ -0,0 +1,6 @@
+name = "Amjad ullah"
+city = "Peshawar"
+hobby = "Coding"
+
+
+print(f"My name is {name} \n City : {city} \n My Hobby is : {hobby}")
