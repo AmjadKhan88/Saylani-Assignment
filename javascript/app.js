fdb8163bf152d42f6418bef9848e761ce92bd5f9
Amjad Ullah <amjadfast87@gmail.com>
2026-06-04 15:11:49 +0500

    Refactor app.js and main.py to simplify output; update package.json to use node for starting the app

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,5 @@
 
-const name = "Amjad ullah";
-const age = 20;
-const favoriteLanguages = ["JavaScript", "Python"];
-function myInfo(){
-    console.log(`Name: ${name}`);
-    console.log(`Age: ${age}`);
-    console.log(`Favorite Programming Languages: ${favoriteLanguages.join(', ')}`);
-}
-myInfo();
+
+console.log("Amjad ullah");
+console.log(20);
+console.log("Javascript");
--- python/main.py
+++ python/main.py
@@ -1,10 +1,4 @@
-# Assignment 1 - Python
-# Displays name, city, and favorite hobby.
 
-name = "Amjad ullah"
-city = "Peshawar"
-hobby = "Coding"
-
-print(f"Name: {name}")
-print(f"City: {city}")
-print(f"Favorite Hobby: {hobby}")
+print("Amjad ullah")
+print("Peshawar")
+print("Coding")
