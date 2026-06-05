93daa5bd5c6d7414bf0cc9f8f3778747b38e528a
Amjad Ullah <amjadfast87@gmail.com>
2026-06-05 19:20:29 +0500

    Add output examples to assignment1.py for clarity on results

--- python/assignment1.py
+++ python/assignment1.py
@@ -9,8 +9,23 @@ print(Age)
 print(City)
 print(isStudent)
 
+'''
+ Output
+ Amjad ullah
+ 20
+ Peshawar
+ True
+'''
+
 
 # Assigment 2 Data types checking
 print(type(Name))
 print(type(Age))
 print(type(isStudent))
+
+'''
+ Output
+ <class 'str'>
+ <class 'int'>
+ <class 'bool'>
+'''
