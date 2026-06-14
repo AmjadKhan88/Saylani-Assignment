35185425854bd1fb9093cf716448de5d0a60127b
Amjad Ullah <amjadfast87@gmail.com>
2026-06-14 11:08:34 +0500

    Add grade_calculater.py to evaluate student grades based on marks

---
+++ python/grade_calculater.py
@@ -0,0 +1,14 @@
+marks = 85
+
+if marks == 100:
+    print("Toper")
+elif marks > 90:
+    print("Grade A+")
+elif marks > 80:
+    print("Grade A")
+elif marks > 70:
+    print("Grade B")
+elif marks > 40:
+    print("Pass")
+else :
+    print("Fail")
