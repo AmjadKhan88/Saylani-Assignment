b5323f31e83fab1abaf4e1aef3a3c2cbbc0f5e25
Amjad Ullah <amjadfast87@gmail.com>
2026-06-14 11:01:37 +0500

    Add assignment.py for Assignment #3 - Python Beginner Level with print statements, variables, and basic arithmetic operations

---
+++ python/assignment.py
@@ -0,0 +1,46 @@
+# Assignment #3 - Python Beginner Level
+
+
+# Task 1: Print Statements
+print("My Name is Amjad")
+print("I am learning Python")
+print("Python is fun!")
+print()
+
+# Task 2: Variables
+name = "Amjad ullah"
+age = 20
+city = "Peshawar"
+
+print(name)
+print(age)
+print(city)
+print()
+
+# Task 3: Addition
+num1 = 15
+num2 = 10
+result_addition = num1 + num2
+print(result_addition)
+print()
+
+# Task 4: Multiplication
+a = 8
+b = 5
+result_multiplication = a * b
+print(result_multiplication)
+print() 
+
+# Task 5: Operators Practice
+print(20 + 10)
+print(20 - 10)
+print(20 * 10)
+print(20 / 10)
+print(20 % 3)
+print(20 // 3)
+print(2 ** 4)
+print() 
+
+# Bonus Task ⭐
+favorite_number = 9
+print(f"My favorite number is {favorite_number}")
